2edd0626b78db9b9dc0af10535ea104756cee512
Pulkit Garg <106508610+Knightride1@users.noreply.github.com>
2025-04-16 15:10:02 +0530

    Update chat_gemini.py

--- chat_gemini.py
+++ chat_gemini.py
@@ -1,7 +1,7 @@
 from urllib import response
 from google import genai
 
-client = genai.Client(api_key="AIzaSyBpgKX-_fCf7unAbbVDwMvmvfv3orB2qPA")
+client = genai.Client(api_key="GEMINI_API_KEY")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Why is the sky blue?"
